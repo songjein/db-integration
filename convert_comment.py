d8a3665c74e46391445cbae3562adb81bf31f3cc
songjein <jeinsong100@gmail.com>
2019-09-23 15:05:53 +0000

    댓글 변환 코드 업데이트 및 모듈화

--- convert_comment.py
+++ convert_comment.py
@@ -10,25 +10,26 @@ import requests
 'created_at': '2016-03-21T01:53:21.000+09:00', 
 'updated_at': '2016-03-21T01:53:21.000+09:00'}
 '''
-with open('comments.json') as f:
-	for line in f:
-		comment = ujson.loads(line)
-		oldId = comment['id']
-		oldPostId = comment['post_id']
-		oldUserId = comment['user_id']
-		body = comment['body']
-		createdAt = comment['created_at']
+def migrate_comment(filepath):
+  with open(filepath) as f:
+    for line in f:
+      comment = ujson.loads(line)
+      oldId = comment['id']
+      oldPostId = comment['post_id']
+      oldUserId = comment['user_id']
+      body = comment['body']
+      createdAt = comment['created_at']
 
-		comment_data = {
-			'oldId': oldId,
-			'oldPostId': oldPostId,
-			'oldUserId': oldUserId,
-			'body': body,
-			'createdAt': createdAt
-		}
+      comment_data = {
+        'oldId': oldId,
+        'oldPostId': oldPostId,
+        'oldUserId': oldUserId,
+        'body': body,
+        'createdAt': createdAt
+      }
 
-		API = 'http://ec2-54-180-96-29.ap-northeast-2.compute.amazonaws.com:3000/comment/integrate'
+      API = 'http://ec2-54-180-96-29.ap-northeast-2.compute.amazonaws.com:3000/comment/integrate'
 
-		res = requests.post(API, data=comment_data)
-		print(res.json())
+      res = requests.post(API, data=comment_data)
+      print(res.json())
 
